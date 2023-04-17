43174b5bd17df80a51c7774c5d338c4d5b210ec1
nastassya-sav <savasya08@gmail.com>
2023-04-17 11:48:14 +0300

    new

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: "app-post",
-    templateUrl: "./post.component.html"
+    templateUrl: "./post.component.html",
+    styleUrls: ['./post.component.scss']
 })
 export class PostComponent {}
